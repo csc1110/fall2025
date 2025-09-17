15f21ae1586f1ec1146778410ca44a8f1c53737b
Dr. Taylor <taylor@msoe.edu>
2025-09-17 09:35:57 -0500

    Week 3 lecture 2

--- src/wk3a/Lec2.java
+++ src/wk3a/Lec2.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class Lec2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int sum = 0;
-        while (in.hasNextInt()) {
-            sum += in.nextInt();
-        }
-        System.out.println(sum);
+        int evenCount = 0;
+        int value;
+        do {
+            value = in.nextInt();
+            if (value > 0 && value % 2 == 0) {
+                evenCount += 1;
+            }
+        } while (value > 0);
+        System.out.println("You entered " + evenCount + " even integers");
     }
 }
