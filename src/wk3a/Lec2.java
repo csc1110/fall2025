b0143b636ebc79ef74e07d73f8c19418a9341f38
Dr. Taylor <taylor@msoe.edu>
2025-09-17 09:24:22 -0500

    Week 3 lecture 2

--- src/wk3a/Lec2.java
+++ src/wk3a/Lec2.java
@@ -1,4 +1,14 @@
 package wk3a;
 
+import java.util.Scanner;
+
 public class Lec2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int sum = 0;
+        while (in.hasNextInt()) {
+            sum += in.nextInt();
+        }
+        System.out.println(sum);
+    }
 }
