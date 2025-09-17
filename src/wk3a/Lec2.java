876652fc699688e01542139265c25c83f7e30862
Dr. Taylor <taylor@msoe.edu>
2025-09-17 09:53:47 -0500

    Week 3 lecture 2

--- src/wk3a/Lec2.java
+++ src/wk3a/Lec2.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 
 public class Lec2 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int evenCount = 0;
-        int value;
-        do {
-            value = in.nextInt();
-            if (value > 0 && value % 2 == 0) {
-                evenCount += 1;
+        String phrase = "This is our last loop type for today.";
+        int numSpaces = 0;
+//        int i;
+//        for (i = 0; i < phrase.length(); i++) {
+//            if (phrase.charAt(i) == ' ') {
+//                numSpaces++;
+//            }
+//        }
+        int i = 0;
+        while (i < phrase.length()) {
+            if (phrase.charAt(i) == ' ') {
+                numSpaces++;
             }
-        } while (value > 0);
-        System.out.println("You entered " + evenCount + " even integers");
+            i++;
+        }
+        System.out.println("Your phrase has " + numSpaces + " space in it");
     }
 }
